1b455bdc1da5f7d7ef168f1e409dc0565cad5aac
Qalib <qurbanowqalib@gmail.com>
2023-01-21 02:44:34 +0400

    ilk commit

---
+++ WinServicesMgr/MainForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WinServicesMgr
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvServices = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ımportExportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ımportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showDescriptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtbDescription = new System.Windows.Forms.RichTextBox();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvServices
+            // 
+            this.lvServices.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lvServices.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvServices.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lvServices.FullRowSelect = true;
+            this.lvServices.GridLines = true;
+            this.lvServices.HideSelection = false;
+            this.lvServices.Location = new System.Drawing.Point(0, 24);
+            this.lvServices.Margin = new System.Windows.Forms.Padding(4);
+            this.lvServices.MultiSelect = false;
+            this.lvServices.Name = "lvServices";
+            this.lvServices.Size = new System.Drawing.Size(709, 301);
+            this.lvServices.TabIndex = 0;
+            this.lvServices.UseCompatibleStateImageBehavior = false;
+            this.lvServices.View = System.Windows.Forms.View.Details;
+            this.lvServices.SelectedIndexChanged += new System.EventHandler(this.lvServices_SelectedIndexChanged);
+            this.lvServices.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvServices_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Display name";
+            this.columnHeader1.Width = 307;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Service name";
+            this.columnHeader2.Width = 256;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Service status";
+            this.columnHeader3.Width = 103;
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Padding = new System.Windows.Forms.Padding(7, 3, 0, 3);
+            this.mainMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.mainMenu.Size = new System.Drawing.Size(709, 24);
+            this.mainMenu.TabIndex = 1;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ımportExportToolStripMenuItem,
+            this.ımportToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 18);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // ımportExportToolStripMenuItem
+            // 
+            this.ımportExportToolStripMenuItem.Name = "ımportExportToolStripMenuItem";
+            this.ımportExportToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.ımportExportToolStripMenuItem.Text = "Export statuses";
+            // 
+            // ımportToolStripMenuItem
+            // 
+            this.ımportToolStripMenuItem.Name = "ımportToolStripMenuItem";
+            this.ımportToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.ımportToolStripMenuItem.Text = "Import statuses";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(150, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showDescriptionToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 18);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // showDescriptionToolStripMenuItem
+            // 
+            this.showDescriptionToolStripMenuItem.Name = "showDescriptionToolStripMenuItem";
+            this.showDescriptionToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.showDescriptionToolStripMenuItem.Text = "Show description";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(43, 18);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // rtbDescription
+            // 
+            this.rtbDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbDescription.Location = new System.Drawing.Point(0, 325);
+            this.rtbDescription.Margin = new System.Windows.Forms.Padding(4);
+            this.rtbDescription.Name = "rtbDescription";
+            this.rtbDescription.Size = new System.Drawing.Size(709, 76);
+            this.rtbDescription.TabIndex = 2;
+            this.rtbDescription.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 401);
+            this.Controls.Add(this.rtbDescription);
+            this.Controls.Add(this.lvServices);
+            this.Controls.Add(this.mainMenu);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.mainMenu;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "WinServicesMgr";
+            this.Load += new System.EventHandler(this.WinServicesMgr_Load);
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvServices;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ımportExportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ımportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showDescriptionToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox rtbDescription;
+    }
+}
+
---
+++ WinServicesMgr/MainForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Management;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace WinServicesMgr
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly ServiceController[] services = ServiceController.GetServices();
+
+        private void WinServicesMgr_Load(object sender, EventArgs e)
+        {
+            lvServices.View = View.Details;
+            lvServices.GridLines = true;
+            lvServices.FullRowSelect = true;
+            lvServices.MultiSelect = false;
+            lvServices.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            lvServices.Sorting = SortOrder.Ascending;
+
+            foreach (ServiceController service in services)
+            {
+                lvServices.Items.Add(new ListViewItem(new string[] { service.DisplayName, service.ServiceName, service.Status.ToString() }));
+            }
+        }
+
+        private void lvServices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lvServices.SelectedIndices.Count > 0)
+                {
+                    string selectedServiceName = lvServices.SelectedItems[0].SubItems[1].Text;
+                    services.ToList().ForEach((service) =>
+                    {
+                        if (service.ServiceName == selectedServiceName)
+                        {
+                            var servis = new ManagementObject(new ManagementPath(string.Format($"Win32_Service.Name='{service.ServiceName}'")));
+                            if (servis["Description"] != null) { rtbDescription.Text = servis["Description"].ToString(); }
+                            else { rtbDescription.Text = ""; }
+                        }
+                    });
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void lvServices_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string selectedServiceName = lvServices.SelectedItems[0].SubItems[1].Text;
+
+            /* Regedit aciq idise, birbawa acilmasini istediyimiz hedef Keye yonlendirib gostere bilmerik, cunki artiq Regedit aciqdir. Yonlendirmemiwden qabaq regediti baglamaliyiq, baglamasaq, aciq qalsa regedit yeni acmaga caliwdigimiz regedit acilmayaq evvelceden aciq olan regedit penceresine fokuslanacayiq. Ona gorede regediti baglayiriq Keyi acmamiwdan qabaq: */
+            foreach (Process proc in Process.GetProcessesByName("regedit")) proc.Kill();
+
+            /* Regedit acilanda fokuslanmagini istediyim Key: */
+            var Path = Registry.LocalMachine.OpenSubKey($"SYSTEM\\CurrentControlSet\\Services\\{selectedServiceName}");
+
+            /* Reyestrda son aciq qalan yolu saxlayan Keydir awagidaki, bu Keyde olan yol acilir Regedit proqrami acilanda. Eger bu keyin deyerini acilmasini istediyimiz Key ile deyiwsek, demeli regedit acilanda son aciq qalan Key olaraq bizim verdiyimiz Keyi bilecek: */
+            Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Applets\Regedit").SetValue("LastKey", Path);
+
+            /* Regedit-i bawlat: */
+            Process.Start("regedit");
+        }
+    }
+}
